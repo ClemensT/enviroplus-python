477f914bc746190b4532311bba61f2508cb9bd55
clemens <clemenstan@gmx.de>
2023-01-17 22:38:23 +0100

    added REST

--- examples/particulates.py
+++ examples/particulates.py
@@ -3,6 +3,19 @@
 import time
 from pms5003 import PMS5003, ReadTimeoutError
 import logging
+import json
+import requests
+
+URL = "http://192.168.178.20:1880/enviro"
+
+
+class Pm:
+    p_1_0 = 0
+    p_2_5 = 0
+    pa_1_0 = 0
+    pa_2_5 = 0
+    p_10 = 0
+
 
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
@@ -22,7 +35,20 @@ try:
     while True:
         try:
             readings = pms5003.read()
-            logging.info(readings)
+            data = Pm()
+            data.p_1_0 = int(readings.pm_ug_per_m3(1.0))
+            data.p_2_5 = int(readings.pm_ug_per_m3(2.5))
+            data.pa_1_0 = int(readings.pm_ug_per_m3(1.0, True))
+            data.pa_2_5 = int(readings.pm_ug_per_m3(2.5, True))
+            data.p_10 = int(readings.pm_ug_per_m3(10.0))
+
+            r = requests.post(url = URL, data = data.__dict__)
+
+            print("works", json.dumps(data.__dict__))
+            print(readings)
+            #logging.info(readings)
+            time.sleep(30.0)
+
         except ReadTimeoutError:
             pms5003 = PMS5003()
 except KeyboardInterrupt:
